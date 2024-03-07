e697ad2f2d4f13c719eba9708936038457436ca0
Headbreakerr <133957591+Headbreakar@users.noreply.github.com>
2024-03-07 14:00:30 +0500

    checking http status

--- javascript/signup.js
+++ javascript/signup.js
@@ -1,9 +1,20 @@
-// signup.js
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector(".signup form");
-  const continueBtn = form.querySelector(".button input");
+const form = document.querySelector(".signup form");
+const continueBtn = form.querySelector(".button input");
 
-  continueBtn.onclick = () => {
-    console.log("hello");
+form.onsubmit = (e) => {
+  e.preventDefault();
+};
+
+continueBtn.onclick = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/signup.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        console.log(data);
+      }
+    }
   };
-});
+  xhr.send();
+};
